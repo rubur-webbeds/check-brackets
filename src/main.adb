993b33c3181449ef4348852247af333f389f3e59
Ruben Bustos <ruben_b_r_@hotmail.com>
2018-03-05 18:41:21 +0100

    use dstack_pointer in main

--- src/main.adb
+++ src/main.adb
@@ -1,3 +1,4 @@
+with Text_IO; use Text_IO;
 with dstack_pointers;
 
 procedure Main is
@@ -5,8 +6,29 @@ procedure Main is
    package CharStack is new dstack_pointers(elem => Character); use CharStack;
 
    my_stack: stack;
+   element: Character;
 
 begin
-   --  Insert code here.
-   null;
+
+   empty(my_stack);
+
+   while not End_Of_Line loop
+
+      Get(element);
+
+      if element = '(' then
+         push(my_stack, element);
+      elsif element = ')' then
+         pop(my_stack);
+      end if;
+   end loop;
+
+   if is_empty(my_stack) then
+      Put_Line("Good");
+   elsif not is_empty(my_stack) then
+      Put_Line("Too many (");
+   end if;
+
+   exception
+         when bad_use => Put_Line("Not enough )");
 end Main;
